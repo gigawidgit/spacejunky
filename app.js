de248b0462f9d83977bfa526909e17d08866b929
WillC.ca <will@willc.ca>
2018-08-24 00:56:58 -0400

    Major Update Includes Three.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const t = (a, b, c) => (c)
-  ? document.querySelector(b || 'div').innerText = a
-  : document.querySelector(b || 'div').innerText += a
-let canvas = document.querySelector('canvas')
-let ctx = canvas.getContext('2d')
+let camera, scene, renderer
+let mesh
+let SCREEN_WIDTH = window.innerWidth
+let SCREEN_HEIGHT = window.innerHeight -
+  document.querySelector('.main').clientHeight
+let screensplit = 1
+let group = new THREE.Group()
+let modifier = new THREE.SubdivisionModifier(1.2)
+let toggle
 let ships = [
   [
     [
@@ -155,154 +159,173 @@ let ships = [
       'booster']],
 
 ]
-let index = 0
-let parts = [
-  'cockpit',
-  'power',
-  'support',
-  'booster',
-  'cannon',
-  'shield',
-  'storage']
-let clrs = ['yellow', 'green', 'purple', 'orange', 'red', 'blue', 'cyan']
-canvas.height = document.querySelector('span').clientHeight
-canvas.width = document.querySelector('span').clientWidth
-let mx = canvas.width / 2
-let my = canvas.height / 2
+let index = 6
+let parts = {
+  'cockpit': {color: 0xFFFF00},
+  'power': {color: 0x00FF0},
+  'support': {color: 0xFF00FF},
+  'booster': {color: 0x3333FF},
+  'cannon': {color: 0xFF0000},
+  'shield': {color: 0x0000FF},
+  'storage': {color: 0xFF3311},
+}
+let clock = new THREE.Clock();
+let pivot = new THREE.Object3D();
+let spin = true
+let ship = ships[index]
+let rotate
 
-function clear () {
-  t('')
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+camera = new THREE.PerspectiveCamera(30, screensplit * SCREEN_WIDTH /
+  SCREEN_HEIGHT, 0.1, 10000)
+camera.position.z = 100
+scene = new THREE.Scene()
+
+let alight = new THREE.AmbientLight(0xCCCCCC) // soft white light
+alight.position.set(0, 0, 1)
+alight.intensity = 0.5
+
+scene.add(alight)
+
+let dlight = new THREE.DirectionalLight(0xFFFFFF)
+dlight.position.set(0, 20, 20)
+dlight.intensity = 0.6
+dlight.shadowMapWidth = 2048
+dlight.shadowMapHeight = 2048
+dlight.castShadow = true
+scene.add(dlight)
+
+renderer = new THREE.WebGLRenderer({antialias: true})
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight - document.querySelector('.main').clientHeight)
+renderer.shadowMap.enabled = true
+document.body.appendChild(renderer.domElement)
+window.addEventListener('resize', onWindowResize, false)
+
+
+
+mixer = new THREE.AnimationMixer( scene );
+
+
+
+
+function init (shipit) {
+  ship = shipit || ships[index]
+  for (let i = pivot.children.length - 1; i >= 0; i--) {
+    pivot.remove(pivot.children[i])
+  }
+  for (let i = group.children.length - 1; i >= 0; i--) {
+    group.remove(group.children[i])
+  }
+  loop();
+  scene.add(group);
+  temp = new THREE.Box3().setFromObject( group );
+  group.position.set(-(Math.abs(temp.max.x+temp.min.x)/2),-(Math.abs(temp.max.y+temp.min.y)/2), 0)
+  pivot.add(group)
+  scene.add(pivot)
 }
 
-const loadShip = (x) => {
-  index = x || 0
-  clear()
-  loop(ships[index])
-
+function onWindowResize () {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
-const color = c => (parts.includes(c))
-  ? ctx.fillStyle = clrs[parts.indexOf(c)]
-  : false
-
-const fill = (s, x, y, xl, yl, ship,list) => {
-  let nx = mx - (xl * 15) / 2
-  let ny = my - (yl * 15) / 2
-  if (!color(s)) {
-    return
-  }
-  ctx.fillRect(nx + 15 + (x * 15), ny + 15 + (y * 15), 10, 10)
-
-  //Connectors
-  if (typeof(ship[x][y + 1]) === 'string') {
-    ctx.fillStyle = 'Turquoise '
-    ctx.fillRect(nx + 15 + (x * 15), ny + 25 + (y * 15), 10, 5)
-  }
-  if (ship[x + 1] && typeof(ship[x + 1][y]) === 'string') {
-    ctx.fillStyle = 'Turquoise '
-    ctx.fillRect(nx + 25 + (x * 15), ny + 15 + (y * 15), 5, 10)
-  }
-
-  //Top Bottom
-  if (!ship[x][y + 1] && s === 'booster') {
-    ctx.fillStyle = 'PaleGoldenRod'
-    ctx.fillRect(nx + 15 + (x * 15), ny + 25 + (y * 15), 10, 15)
-  }
-  if (!ship[x][y - 1] && s === 'booster') {
-    ctx.fillStyle = 'PaleGoldenRod'
-    ctx.fillRect(nx + 15 + (x * 15), ny + 25 + (y * 15) - 25, 10, 15)
-  }
-  //Left Right
-  if ((!ship[x + 1] || !ship[x + 1][y]) && s === 'booster') {
-
-    ctx.fillStyle = 'PaleGoldenRod'
-    ctx.fillRect(nx + 25 + (x * 15), ny + 15 + (y * 15), 15, 10)
-  }
-  if ((!ship[x - 1] || !ship[x - 1][y]) && s === 'booster') {
-    ctx.fillStyle = 'PaleGoldenRod'
-    ctx.fillRect(nx + (x * 15), ny + 15 + (y * 15), 15, 10)
-  }
-
-  if (s === 'cannon') {
-
-    let h = []
-    let v = []
-    for (let a = 0; a < ship.length; a++) { h.push(ship[a][y]) }
-    for (let b = 0; b < ship[x].length; b++) { v.push(ship[x][b]) }
-
-    let t = v.slice(0, v.indexOf('cannon')-1).
-      filter(j => j !== false || j !== undefined)
-
-    let b = v.slice(v.lastIndexOf('cannon'), v.length-1).
-      filter(j => j !== false || j !== undefined)
-
-    let l = h.slice(0, h.indexOf('cannon')-1).
-      filter(j => j !== false || j !== undefined)
-
-    let r = h.slice(v.lastIndexOf('cannon'), h.length-1).
-      filter(j => j !== false || j !== undefined)
-
-
-    if (ship[x][y + 1] && ship[x + 1] && ship[x + 1][y] && ship[x - 1] && ship[x - 1][y] && ship[x][y - 1]) {
-      return
-    }
-
-    if ((!y && !t.length) || !y || (!ship[x][y-1] && !t.length)){
-      ctx.fillStyle = 'pink'
-      ctx.fillRect(nx + 15 + (x * 15), ny + 25 + (y * 15) - 25, 10, 15)
-      return
-    }
-
-    if ((!y && !b.length) || (!ship[x][y+1] && ship[x][y-1])) {
-      ctx.fillStyle = 'pink'
-      ctx.fillRect(nx + 15 + (x * 15), ny + 25 + (y * 15), 10, 15)
-      return
-    }
-
-    if ((x === xl-1 && !r.length) || (x === xl-1 && !r.length) || x===xl-1) {
-      ctx.fillStyle = 'pink'
-      ctx.fillRect(nx + 25 + (x * 15), ny + 15 + (y * 15), 15, 10)
-      return
-    }
-
-      ctx.fillStyle = 'pink'
-      ctx.fillRect(nx + (x * 15), ny + 15 + (y * 15), 15, 10)
-
-
-
-
-
-
-  }
+function animate () {
+  requestAnimationFrame(animate)
+  render()
 }
-const loop = (ship) => {
-  ship.forEach((a, x) => {
+
+function makeBox (x, y, c, w, h, d, m, r) {
+  let cyl = (!m)
+    ? new THREE.BoxGeometry(w || 1, h || 1, 1, 2, 2, 2)
+    : new THREE.CylinderGeometry(w || 1, w || 1, h || 1)
+  let cyl_m = new THREE.MeshPhongMaterial({color: c})
+  let smooth = (!m) ? modifier.modify(cyl) : cyl
+
+  let mesh = new THREE.Mesh(smooth, cyl_m)
+  mesh.castShadow = true
+  mesh.receiveShadow = true
+  if (r) mesh.rotation.z = Math.PI / 2
+
+  mesh.position.set(x || 0, y || 0, 0)
+  return mesh
+}
+
+const color = c => parts[c].color || false
+
+const loop = (ls, x) => {
+  x = x || 0
+  ls = ls || ship
+  ls.forEach((a, y) => {
     if (typeof(a) === 'object') {
-      a.forEach((b, y) => {
-        fill(b, x, y, ship.length, 1, ship, a.length)
-      })
+      loop(a, y)
     }
     else {
-      fill(a, x, 1, ship.length, ship.length, ship, [1])
+      if (typeof(a) === 'string') {
+        group.add(makeBox(x*1.1, y*1.1, color(a)))
+        if (ship[x + 1] && typeof(ship[x + 1][y]) === 'string') {
+          group.add(
+            makeBox((x*1.1) + 0.3, y*1.1, 'white', 0.3, 1.2, 1, true, true))
+        }
+        if (ship[x][y] && typeof(ship[x][y + 1]) === 'string') {
+          group.add(makeBox(x*1.1, (y*1.1) + 0.3, 'white', 0.3, 1.2, 1, true))
+        }
+      }
+
     }
   })
+
+
+}
+init()
+animate()
+
+
+function render() {
+  let delta = 0.75 * clock.getDelta();
+    if(rotate === 'left') {
+      pivot.rotation.z += 0.04
+    }
+    if(rotate === 'right'){
+      pivot.rotation.z -= 0.04
+    }
+
+  mixer.update( delta );
+  renderer.render( scene, camera );
 }
 
-document.querySelector('.next').addEventListener('click', () => {
-  loadShip(((index < ships.length - 1) ? index + 1 : 0))
-})
-document.querySelector('.prev').addEventListener('click', () => {
-  loadShip(((index >= 1) ? index - 1 : ships.length - 1))
-})
-document.querySelector('.build').addEventListener('click', () => {
-  clear()
 
-  loop(JSON.parse(JSON.stringify(document.querySelector('input').
-    value.
-    split('|').
-    map(a => a.split(',').map(b => ((!b.length) ? false : b.trim()))))))
+
+window.addEventListener('keydown',(a)=>{
+  if(a.key==='ArrowLeft'){
+      rotate = 'left'
+  }
+  if(a.key==='ArrowRight'){
+    rotate = 'right'
+  }
 
 })
+window.addEventListener('keyup',(a)=> {
+rotate = false
+})
 
-loadShip()
+
+// document.querySelector('.next').addEventListener('click', () => {
+//   index = (index < ships.length - 1) ? index + 1 : 0
+//   init()
+// })
+// document.querySelector('.prev').addEventListener('click', () => {
+//   index = (index >= 1) ? index - 1 : ships.length - 1
+//   init()
+// })
+// document.querySelector('.build').addEventListener('click', () => {
+//   ship = JSON.parse(JSON.stringify(document.querySelector('.inputs').
+//     value.
+//     split('|').
+//     map(a => a.split(',').map(b => ((!b.length) ? false : b.trim())))))
+//   init(ship)
+// })
+// document.querySelector('.animate').addEventListener('click', () => {
+//   spin = !spin
+// })
